a580677be3d1d32d41f99cac4df23df1186cfa07
emassaki <massakihirayama@gmail.com>
2021-03-22 18:44:25 -0300

    Updates

---
+++ precache-manifest.ad86bdcdb62a45f794fa813a2b4e63b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2288b8b4d6e9a7e5e261448a5fef9d8d",
+    "url": "/trybe-wallet/index.html"
+  },
+  {
+    "revision": "97847d29135a4a35188d",
+    "url": "/trybe-wallet/static/css/main.5d1ad455.chunk.css"
+  },
+  {
+    "revision": "80b29c5d4e298499b96c",
+    "url": "/trybe-wallet/static/js/2.e11492c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-wallet/static/js/2.e11492c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97847d29135a4a35188d",
+    "url": "/trybe-wallet/static/js/main.de2fe850.chunk.js"
+  },
+  {
+    "revision": "d390f7f33f2ac1ae0bcc",
+    "url": "/trybe-wallet/static/js/runtime-main.f94807e2.js"
+  }
+]);
